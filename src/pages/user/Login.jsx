91dc122f9fe4aa3151f0cc98ae53490e1c8e3819
nabeelnaeem <nnabeel03@gmail.com>
2025-07-11 19:58:22 +0500

    Some const changes in Login.jsx

--- src/pages/user/Login.jsx
+++ src/pages/user/Login.jsx
@@ -2,6 +2,16 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Login = () => {
+    //Messages
+    const DONT_HAVE_ACCOUNT_MSG = "Don't have an account?";
+
+    //classNames
+    const INPUT_LABEL_CLASS = "block mb-1";
+    const INPUT_CLASS = "w-full border border-gray-50-300 p-2";
+    const BUTTON_CLASS = "w-full bg-blue-600 text-white py-2 rounded hover:bg-blue-500";
+    const SIGNUP_CLASS = "text-blue-600 hover:underline";
+    //variables
+
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
@@ -14,20 +24,20 @@ const Login = () => {
             <h2 className=" text-xl font-semibold mb-4">Login</h2>
             <form className=" space-y-4" onSubmit={handleLogin}>
                 <div>
-                    <label className=" block mb-1">User Name:</label>
+                    <label className={INPUT_LABEL_CLASS}>User Name:</label>
                     <input
                         type="text"
-                        className="w-full border border-gray-50-300 p-2"
+                        className={INPUT_CLASS}
                         value={username}
                         onChange={(e) => setUsername(e.target.value)}
                         required
                     />
                 </div>
                 <div>
-                    <label className=" block mb-1">Password:</label>
+                    <label className={INPUT_LABEL_CLASS}>Password:</label>
                     <input
                         type="password"
-                        className="w-full border border-gray-50-300 p-2"
+                        className={INPUT_CLASS}
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                         required
@@ -35,14 +45,14 @@ const Login = () => {
                 </div>
                 <button
                     type="submit"
-                    className="w-full bg-blue-600 text-white py-2 rounded hover:bg-blue-500"
+                    className={BUTTON_CLASS}
                 >
                     Login
                 </button>
             </form>
             <p className="mt-4 text-center">
-                Don't have an account?{" "}
-                <Link to="/signup" className="text-blue-600 hover:underline">
+                {DONT_HAVE_ACCOUNT_MSG}
+                <Link to="/signup" className={SIGNUP_CLASS}>
                     Sign up
                 </Link>
             </p>
