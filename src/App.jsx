f6fe1fccc6192937400f4fac84444ed769dd8538
nabeelnaeem <nnabeel03@gmail.com>
2025-07-11 20:00:29 +0500

    Small update to App.jsx - Included Footer

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
-
+      <Footer />
     </BrowserRouter>
   )
 }
