72f7e3a297e259dd0cbd8356b360920559f1d35b
nabeelnaeem <nnabeel03@gmail.com>
2025-07-11 16:35:40 +0500

    Installed tailwindcss and update some initial files

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,7 +6,9 @@ function App() {
 
   return (
     <>
-      <div>Ecommerce Store</div>
+      <h1 class="text-3xl font-bold underline">
+        Ecommerce Store
+      </h1>
     </>
   )
 }
