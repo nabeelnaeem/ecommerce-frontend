22b74dee6e4e88a32619e96155762150fec626fe
nabeelnaeem <nnabeel03@gmail.com>
2025-07-11 17:05:17 +0500

    Added navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+const Navbar = () => {
+    return (
+        <nav>
+            <ul className=" flex space-x-4">
+                <li>
+                    <Link to="/" className=" hover:underline">Home</Link>
+                </li>
+            </ul>
+            <ul className=" flex space-x-4">
+                <li>
+                    <Link to="/store" className=" hover:underline">Store</Link>
+                </li>
+            </ul>
+            <ul className=" flex space-x-4">
+                <li>
+                    <Link to="/products" className=" hover:underline">Products</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
