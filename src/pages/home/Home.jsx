1f25d50d39645fbfaed897c3bb8a46101d710882
nabeelnaeem <nnabeel03@gmail.com>
2025-07-11 17:17:12 +0500

    Created Home.jsx

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+const Home = () => {
+    return (
+        <main className=" p-6">
+            <h2 className=" text-2xl font-bold mb-4">
+                Welcome to Ecommerce Store!
+            </h2>
+            <p>
+                Discover our products and enjoy shopping.
+            </p>
+        </main>
+    )
+}
