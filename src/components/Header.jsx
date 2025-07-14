292435fb44e84ec2ba0cf50e59ac0fc42b8a900c
nabeelnaeem <nnabeel03@gmail.com>
2025-07-14 19:49:22 +0500

    Header.jsx fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,32 +4,42 @@ import { STORE_NAME } from '../config/store-config.js'
 import { useAuth } from '../context/AuthContext'
 
 const Header = () => {
+    //Classes
+    const HEADER_CLASS = "bg-gray-800 text-white p-4 flex justify-between items-center";
+    const LOGO_TITLE_CLASS = " text-xl font-semibold";
+    const USERNAME_CLASS = "flex items-center gap-4";
+    const LOGOUT_BUTTON_CLASS = "bg-red-600 hover:bg-red-500 text-white px-4 py-1 rounded";
+    const LOGIN_BUTTON_CLASS = "bg-blue-600 hover:bg-blue-500 text-white px-4 py-1 rounded";
+
+
+    //State Variables
     const { user, logout } = useAuth();
 
     return (
-        <header className="bg-gray-800 text-white p-4 flex justify-between items-center">
+        <header className={HEADER_CLASS}>
             {/*Logo/Branding*/}
-            <h1 className=" text-xl font-semibold">
+            <h1 className={LOGO_TITLE_CLASS}>
                 <Link to="/">{STORE_NAME}</Link>
             </h1>
             {/*Navigation Links*/}
             <Navbar />
             {user ? (
-                <div className="flex items-center gap-4">
+                <div className={USERNAME_CLASS}>
                     <span>Hello, {user}</span>
                     <button
                         onClick={logout}
-                        className="bg-red-500 hover:bg-red-400 text-white px-4 py-1 rounded"
+                        className={LOGOUT_BUTTON_CLASS}
                     >
                         Logout
                     </button>
                 </div>
             ) : (
-                <Link to="/login" className="bg-blue-600 hover:bg-blue-500 text-white px-4 py-2 rounded">
+                <Link to="/login" className={LOGIN_BUTTON_CLASS}>
                     Login
                 </Link>
-            )}
-        </header>
+            )
+            }
+        </header >
     )
 }
 export default Header; 
