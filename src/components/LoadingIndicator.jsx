28712d06bc495addc37b2dfa780cf93c54b8e7b3
nabeelnaeem <nnabeel03@gmail.com>
2025-07-15 14:25:29 +0500

    Updated LoadingIndicator.jsx

--- src/components/LoadingIndicator.jsx
+++ src/components/LoadingIndicator.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { LOADING_CLASS } from '../styles/styles';
 
+// Loading Indicator Classes
+const LOADING_CONTAINER = "flex justify-center items-center py-12";
+
 const LoadingIndicator = () => {
     return (
-        <div className="flex justify-center items-center py-12">
+        <div className={LOADING_CONTAINER}>
             <div className={LOADING_CLASS}></div>
         </div>
     );
